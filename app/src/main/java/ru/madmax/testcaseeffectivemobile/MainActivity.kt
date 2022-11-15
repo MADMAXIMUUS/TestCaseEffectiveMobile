95a90ac2987fa2ed868420bd67a94fe96150126a
MADMAXIMUUS <maxim00nechaev@gmail.com>
2022-11-15 21:18:35 +0300

    Add main and filter fragments views

--- app/src/main/java/ru/madmax/testcaseeffectivemobile/MainActivity.kt
+++ app/src/main/java/ru/madmax/testcaseeffectivemobile/MainActivity.kt
@@ -3,7 +3,6 @@ package ru.madmax.testcaseeffectivemobile
 import android.content.pm.ActivityInfo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
